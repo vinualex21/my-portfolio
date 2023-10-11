dd0928ac8751c3ac0339a0c87d4e3b837fd186f6
vinualex21 <alexvinu2001@gmail.com>
2023-10-11 12:03:23 +0100

    Add page for work experience and education
    - Add material ui icons package
    - Add navigation link in the sidebar menu
    - Add component for experience
    - Fix styling in Projects component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Contact from './components/Contact';
 import Home from './components/Home';
 import Layout from './components/Layout';
 import Projects from './components/Projects';
+import Experience from './components/Experience';
 import {Route, Routes} from 'react-router-dom';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
         </Route>
       </Routes>
     </>
